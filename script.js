e4cfb2519d2586070ccec154a2d964d81f994de5
RodrigoStaMaria <>
2021-05-08 16:22:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+const elementoTitulo = document.getElementById("titulo")
+
+const elementoVideo = document.getElementById("TextoVideo")
+
+const elementoLink = document.getElementById("TextoFacebook")
+
+
+elementoTitulo.innerHTML = "Titulo modificado"
+
+elementoVideo.innerHTML = "Me gusta ver los videos de WillyRex porque me divierte"
+
+elementoLink.innerHTML = "A la pagina de facebook"
+
+
+elementoLink.style.color = "Brown"
+
+elementoTitulo.style.color = "Green"
+
+elementoVideo.style.color = "blue"
+
+elementoTitulo.style.fontSize = "x-large"
+
+
+let parent = document.getElementById("container")
+
+//parent.removeChild(parent.firstElementChild)
+
